ce2f15cd1397ae1d81c0db1108c4cfdd265d7306
aryamaahendra <arya.mahendra30k@gmail.com>
2023-12-05 19:57:16 +0800

    fix active navbar

--- resources/views/components/layouts/dashboard/partials/navbar-main-menu.blade.php
+++ resources/views/components/layouts/dashboard/partials/navbar-main-menu.blade.php
@@ -1,5 +1,8 @@
 @php
     $rn = Route::currentRouteName();
+    $s_one = request()->segment(1) ?? '';
+    $s_two = request()->segment(2) ?? '';
+    $s_three = request()->segment(3) ?? '';
 @endphp
 
 <x-preline.navbar.menu-item-2 href="{{ route('dshb.index') }}" :active="$rn == 'dshb.index'">
@@ -7,17 +10,17 @@
     Dashboard
 </x-preline.navbar.menu-item-2>
 
-<x-preline.navbar.menu-item-2 href="{{ route('dshb.produk.index') }}" :active="$rn == 'dshb.produk.index'">
+<x-preline.navbar.menu-item-2 href="{{ route('dshb.produk.index') }}" :active="$s_two == 'produk'">
     <x-icons.packege class="w-5 h-5" />
     Produk
 </x-preline.navbar.menu-item-2>
 
-<x-preline.navbar.menu-item-2 href="{{ route('dshb.restock.index') }}" :active="$rn == 'dshb.restock.index'">
+<x-preline.navbar.menu-item-2 href="{{ route('dshb.restock.index') }}" :active="$s_two == 'restock'">
     <x-icons.truck class="w-5 h-5" />
     Re-Stock
 </x-preline.navbar.menu-item-2>
 
-<x-preline.navbar.menu-item-2 href="{{ route('dshb.material-terpakai.index') }}" :active="$rn == 'dshb.material-terpakai.index'">
+<x-preline.navbar.menu-item-2 href="{{ route('dshb.material-terpakai.index') }}" :active="$s_two == 'material-terpakai'">
     <x-icons.assembly class="w-5 h-5" />
     Metrial Terpakai
 </x-preline.navbar.menu-item-2>
@@ -28,16 +31,20 @@
 </x-preline.navbar.menu-item-2>
 
 @if (Role::admin())
-    <x-preline.navbar.menu-dropdown-2 label="Master" :active="in_array($rn, ['dshb.bahan-baku.index', 'dshb.supplier.index', 'dshb.user.index'])">
-        <x-preline.dropdown.item href="{{ route('dshb.bahan-baku.index') }}" :active="$rn == 'dshb.bahan-baku.index'">
+    <x-preline.navbar.menu-dropdown-2 label="Master" :active="in_array($s_two, [
+        'bahan-baku',
+        'supplier',
+        'user',
+    ])">
+        <x-preline.dropdown.item href="{{ route('dshb.bahan-baku.index') }}" :active="$s_two == 'bahan-baku'">
             Bahan Baku
         </x-preline.dropdown.item>
 
-        <x-preline.dropdown.item href="{{ route('dshb.supplier.index') }}" :active="$rn == 'dshb.supplier.index'">
+        <x-preline.dropdown.item href="{{ route('dshb.supplier.index') }}" :active="$s_two == 'supplier'">
             Supplier
         </x-preline.dropdown.item>
 
-        <x-preline.dropdown.item href="{{ route('dshb.user.index') }}" :active="$rn == 'dshb.user.index'">
+        <x-preline.dropdown.item href="{{ route('dshb.user.index') }}" :active="$s_two == 'user'">
             Pengguna
         </x-preline.dropdown.item>
     </x-preline.navbar.menu-dropdown-2>
